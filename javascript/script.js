67447f7c1f26eeec5438771ee99216bc80c33c5c
AdeAde99 <adelintanasa.d@gmail.com>
2022-03-14 17:14:12 +0100

    milestone 3

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        arrToDo: [
+            {
+                text: 'aiuto',
+            },
+        ],
+    },
+    methods: {
+
+        pushObj() {
+            let text = document.querySelector('input').value;
+            const todo = {
+                text : text,
+                done: false
+            }
+            this.arrToDo.push(todo);
+            console.log(todo);
+        }
+    }
+});
